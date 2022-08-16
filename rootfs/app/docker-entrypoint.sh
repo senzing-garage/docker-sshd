4efb6339d4440a2bcd6c761b731a9e069c00136f
roncewind <dad@lynntribe.net>
2022-08-16 07:26:31 -0700

    #112 updated env var handling

--- rootfs/app/docker-entrypoint.sh
+++ rootfs/app/docker-entrypoint.sh
@@ -6,7 +6,10 @@ then
 	> /etc/motd
 fi
 
+# when the root password is set externally, update the password
 echo root:${ROOT_PASSWORD} | chpasswd
+# then remove the root password as and env var
+echo "unset ROOT_PASSWORD" >> /etc/profile
 echo "export SENZING_ENGINE_CONFIGURATION_JSON='${SENZING_ENGINE_CONFIGURATION_JSON}'" >> /etc/profile
 echo "export SENZING_SKIP_DATABASE_PERFORMANCE_TEST=${SENZING_SKIP_DATABASE_PERFORMANCE_TEST}" >> /etc/profile
 /usr/sbin/sshd -D
